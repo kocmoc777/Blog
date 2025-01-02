28fb62aa93ed8eff739b6c02f27aa1abaa83b292
kocmoc777 <150328388+kocmoc777@users.noreply.github.com>
2025-01-02 18:31:26 +0200

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -148,8 +148,6 @@
             </div>
         </div>
         <div class="footer-text">
-            <p class="mb-md-0">©  2020, oleez Theme. Made with passion by <a href="https://www.bootstrapdash.com" target="_blank" rel="noopener noreferrer" class="text-reset">BootstrapDash</a>.</p>
-            <p class="mb-0">All right reserved.</p>
         </div>
     </div>
 </footer>
